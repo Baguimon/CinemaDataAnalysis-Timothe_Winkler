1e875014c848e7eba7c228dc8e8149867a1736cf
Baguimon <timothe.winkler@gmail.com>
2025-01-12 17:07:13 +0100

    Exercice 4 - 2. Entraînez un modèle de régression linéaire

--- exercice4.py
+++ exercice4.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
 
 # Charge les données
 file_path = "data/cinemas.csv"
@@ -13,3 +16,16 @@ df_2021.columns = ['ecrans', 'fauteuils', 'population_commune', 'entrees_annuell
 
 print("Aperçu des données pour 2021 :")
 print(df_2021.head())
+
+# Sépare les variables explicatives (X) et la variable cible (y)
+X = df_2021[['ecrans', 'fauteuils', 'population_commune']]
+y = df_2021['entrees_annuelles']
+
+# Divise les données en ensembles d'entraînement (80%) et de test (20%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Créer et entraîner le modèle de régression linéaire
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print("Modèle de régression entraîné.")
