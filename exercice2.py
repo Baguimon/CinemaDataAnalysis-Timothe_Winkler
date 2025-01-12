ddbf61593a38d936ddfb66454133448d625f0101
Baguimon <timothe.winkler@gmail.com>
2025-01-12 12:32:34 +0100

    Exercice 2 - 2. Les 3 meilleures régions et les 3 pires

--- exercice2.py
+++ exercice2.py
@@ -12,3 +12,14 @@ region_stats = df.groupby('region administrative')['entrees_par_fauteuil_2022'].
 
 # Affiche les entrées moyennes par fauteuil pour chaque région en 2022
 print(region_stats)
+
+# Trie des entrées moyennes par fauteuil de 2022 
+region_stats_sorted = region_stats.sort_values(by='entrees_par_fauteuil_2022', ascending=False)
+
+# Affiche les 3 meilleures régions
+print("\n3 Top 3 Meilleures régions (entrées moyennes par fauteuil) :")
+print(region_stats_sorted.head(3))
+
+# Affiche les 3 pires régions
+print("\n3 Top 3 Pires régions (entrées moyennes par fauteuil) :")
+print(region_stats_sorted.tail(3))
