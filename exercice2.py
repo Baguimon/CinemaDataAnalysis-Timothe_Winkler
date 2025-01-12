3bd699f9abbffd964401a275f28823ac8ae1afbb
Baguimon <timothe.winkler@gmail.com>
2025-01-12 13:11:39 +0100

    Exercice 2 - Complet

--- exercice2.py
+++ exercice2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Charge les données
 file_path = "data/cinemas.csv"
@@ -23,3 +24,21 @@ print(region_stats_sorted.head(3))
 # Affiche les 3 pires régions
 print("\n3 Top 3 Pires régions (entrées moyennes par fauteuil) :")
 print(region_stats_sorted.tail(3))
+
+
+# Création d'un top 10 des régions
+top_10_regions = region_stats_sorted.head(10)
+
+# Création du graphique
+plt.figure(figsize=(10, 6))
+plt.bar(top_10_regions['region administrative'], top_10_regions['entrees_par_fauteuil_2022'], color='skyblue') 
+
+# Ajout des labels et du titre
+plt.xlabel('Régions')
+plt.ylabel('Entrées moyennes par fauteuil')
+plt.title('Entrées moyennes par fauteuil pour les 10 premières régions en 2022')
+plt.xticks(rotation=45, ha='right')  # Pour mieux afficher les labels des régions
+
+# Afficher le graphique
+plt.tight_layout()  # Ajustement pour éviter les chevauchements
+plt.show()
