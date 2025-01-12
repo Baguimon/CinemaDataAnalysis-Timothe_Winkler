0b4a893de5845a80dc5061ae2f38819fa8436ab4
Baguimon <timothe.winkler@gmail.com>
2025-01-12 16:03:08 +0100

    Exercice 3 - 2. Calcul des corrélations

--- exercice3.py
+++ exercice3.py
@@ -6,3 +6,10 @@ df = pd.read_csv(file_path)
 
 # Filtre pour garder uniquement l'année 2022
 df_2022 = df[df['entrees 2022'].notna()]
+
+# Calcul des corrélations
+correlation_ecrans = df_2022['ecrans'].corr(df_2022['entrees 2022'])
+correlation_fauteuils = df_2022['fauteuils'].corr(df_2022['entrees 2022'])
+
+print(f"Corrélation entre le nombre d'écrans et les entrées annuelles (2022) : {correlation_ecrans:.2f}")
+print(f"Corrélation entre le nombre de fauteuils et les entrées annuelles (2022) : {correlation_fauteuils:.2f}")
